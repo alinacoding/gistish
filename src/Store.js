a971e4caf84312efe2acc6fda5802cd0741b7a99
Alina Voicu <a.voicu24@gmail.com>
2022-08-24 01:28:34 +0300

    Store cleanup

--- src/Store.js
+++ src/Store.js
@@ -1,28 +1,7 @@
 import { React, useState, useEffect, useContext } from "react";
-import SearchWrapper from "./components/SearchWrapper/SearchWrapper";
-import {
-  StoreContext,
-  StoreContextProvider,
-} from "./contexts/StoreContext/StoreContext";
-// import  from "./contexts/StoreContext/StoreContext";
-const Store = (props) => {
-  console.log(StoreContext);
-  const {
-    submittedButtonVS,
-    queryUserVS,
-    queryUserChangedVS,
-    paginateVS,
-    errorVS,
-    gistsVS,
-  } = useContext(StoreContext);
-  console.log(submittedButtonVS);
-  //const [submittedButtonValue, setSubmittedButtonValue] = submittedButtonVS;
-  //   const [queryUserValue, setQueryUserValue] = queryUserVS;
-  //   const [queryUserChangedValue, setQueryUserChangedValue] = queryUserChangedVS;
-  //   const [paginateValue, setPaginateValue] = paginateVS;
-  //   const [errorValue, setErrorValue] = errorVS;
-  //   const [gistsValue, setGistsValue] = gistsVS;
+import { StoreContextProvider } from "./contexts/StoreContext/StoreContext";
 
+const Store = (props) => {
   return <StoreContextProvider>{props.children}</StoreContextProvider>;
 };
 
