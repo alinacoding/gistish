a853a00c13429ed6ec2b5685ae9a88472b4ca702
Alina Voicu <a.voicu24@gmail.com>
2022-08-24 11:30:27 +0300

    Refactor

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect, useContext } from "react";
+import { React } from "react";
 import { StoreContextProvider } from "./contexts/StoreContext/StoreContext";
 
 const Store = (props) => {
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppWrapper from "./AppWrapper";
 import Store from "./Store";
-
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppWrapper>
-      <Store />
-    </AppWrapper>
+    <Store>
+      <App />
+    </Store>
   </React.StrictMode>
 );
 
